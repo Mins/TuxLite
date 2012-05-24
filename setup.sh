31cd30af8352e1679a841f5685e951fe7942ef04
Stefan Crain <stefancrain@gmail.com>
2012-05-24 14:13:09 -0400

    Added installer for lsb-release as it is required on mini instlals
    Because DISTRO is referenced in 2 functions, i placed the install
    lsb-release command at the top of the function.

--- setup.sh
+++ setup.sh
@@ -8,11 +8,14 @@
 
 source ./options.conf
 
+
+aptitude -y install lsb-release
 # Operating system
-OS=`lsb_release -i -s`
+DISTRO=`lsb_release -i -s`
 # Release
-R=`lsb_release -c -s`
+RELEASE=`lsb_release -c -s`
 #### Functions Begin ####
+
 
 function basic_server_setup {
 
@@ -26,8 +29,11 @@ function basic_server_setup {
     # Set hostname and FQDN
     sed -i 's/'${SERVER_IP}'.*/'${SERVER_IP}' '${HOSTNAME_FQDN}' '${HOSTNAME}'/' /etc/hosts
     echo "$HOSTNAME" > /etc/hostname
+    if  [ $DISTRO = ""]; then
+        echo -e "\033[35;1m  You need to run the setup_apt function first, or 'aptitude -y install lsb-release' \033[0m"
+    fi 
 
-    if [ $OS = "Debian"]; then
+    if [ $DISTRO = "Debian"]; then
         # Debian system, use hostname.sh
         service hostname.sh start
     else
@@ -48,52 +54,53 @@ function basic_server_setup {
 
 
 function setup_apt {
+  
 
     cp /etc/apt/{sources.list,sources.list.bak}
 
-    if [ $OS  == "Debian" ]; then
+    if [ $DISTRO  == "Debian" ]; then
         echo -e "\033[35;1m Its Debian \033[0m"
         # Debian system, use Debian sources.list
         cat > /etc/apt/sources.list <<EOF
 # Main repo
-deb http://ftp.$APT_REGION.debian.org/debian $R main non-free contrib
-deb-src  http://ftp.$APT_REGION.debian.org/debian $R main non-free contrib
+deb http://ftp.$APT_REGION.debian.org/debian $RELEASE main non-free contrib
+deb-src  http://ftp.$APT_REGION.debian.org/debian $RELEASE main non-free contrib
 
 # Security
-deb http://security.debian.org/ $R/updates main contrib non-free
-deb-src http://security.debian.org/ $R/updates main contrib non-free
+deb http://security.debian.org/ $RELEASE/updates main contrib non-free
+deb-src http://security.debian.org/ $RELEASE/updates main contrib non-free
 
 EOF
 
     else 
-        if [ $OS  == "Ubuntu" ]; then 
+        if [ $DISTRO  == "Ubuntu" ]; then 
         echo -e "\033[35;1m Its Ubuntu \033[0m"
     # Otherwise use Ubuntu sources.list
 
         cat > /etc/apt/sources.list <<EOF
 # Main repo
-deb http://$APT_REGION.archive.ubuntu.com/ubuntu/ $R main restricted universe multiverse
-deb-src http://$APT_REGION.archive.ubuntu.com/ubuntu/ $R main restricted universe
+deb http://$APT_REGION.archive.ubuntu.com/ubuntu/ $RELEASE main restricted universe multiverse
+deb-src http://$APT_REGION.archive.ubuntu.com/ubuntu/ $RELEASE main restricted universe
 
 # Security & updates
-deb http://$APT_REGION.archive.ubuntu.com/ubuntu/ $R-updates main restricted universe multiverse
-deb-src http://$APT_REGION.archive.ubuntu.com/ubuntu/ $R-updates main restricted universe
-deb http://security.ubuntu.com/ubuntu $R-security main restricted universe
-deb-src http://security.ubuntu.com/ubuntu $R-security main restricted universe
+deb http://$APT_REGION.archive.ubuntu.com/ubuntu/ $RELEASE-updates main restricted universe multiverse
+deb-src http://$APT_REGION.archive.ubuntu.com/ubuntu/ $RELEASE-updates main restricted universe
+deb http://security.ubuntu.com/ubuntu $RELEASE-security main restricted universe
+deb-src http://security.ubuntu.com/ubuntu $RELEASE-security main restricted universe
 
 EOF
         else
              
-            if [ $OS  != "Ubuntu" ] && [ $OS  != "Debian"  ]; then
+            if [ $DISTRO  != "Ubuntu" ] && [ $DISTRO  != "Debian"  ]; then
                 # throw an error if its not supported os, 
-                echo -e "\033[35;1m Sorry, Distro: '"$OS"' and Release: '"$R"' are not supported at this time. \033[0m"
+                echo -e "\033[35;1m Sorry, Distro: '"$DISTRO"' and Release: '"$RELEASE"' are not supported at this time. \033[0m"
                 exit
             fi
         fi # End if 
     fi
     # Need to add Dotdeb repo for PHP5-FPM when using Debian 6.0
-    if [ $R = "squeeze" ]; then
-         echo -e "\033[35;1m Its Debian '"$R"' \033[0m"
+    if [ $RELEASE = "squeeze" ]; then
+         echo -e "\033[35;1m Its Debian '"$RELEASE"' \033[0m"
         cat >> /etc/apt/sources.list <<EOF
 # Dotdeb
 deb http://packages.dotdeb.org stable all
